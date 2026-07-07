4c959e207c256793a02a4471cb1334c346ba3dcf
AlirezaDaraei <daraeialireza76@gmail.com>
2026-07-08 01:09:58 +0330

    add registerRequestToAuthUser to Mapper and improve toAuthResponse method name

--- authentication-service/src/main/java/org/dara/authenticationservice/mapper/AuthUserMapper.java
+++ authentication-service/src/main/java/org/dara/authenticationservice/mapper/AuthUserMapper.java
@@ -1,11 +1,13 @@
 package org.dara.authenticationservice.mapper;
 
 import org.dara.authenticationservice.dto.AuthResponse;
+import org.dara.authenticationservice.dto.RegisterRequest;
 import org.dara.authenticationservice.model.AuthUser;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface AuthUserMapper {
 
-    public AuthResponse toAuthResponse(AuthUser authUser);
+    public AuthResponse authUserToAuthResponse(AuthUser authUser);
+    public AuthUser registerRequestToAuthUser(RegisterRequest registerRequest);
 }
