6480af81b38838c25174c31a04445ac25716e72d
AlirezaDaraei <daraeialireza76@gmail.com>
2026-07-08 01:13:17 +0330

    complete Role service and repository methods

--- authentication-service/src/main/java/org/dara/authenticationservice/service/RoleService.java
+++ authentication-service/src/main/java/org/dara/authenticationservice/service/RoleService.java
@@ -1,4 +1,10 @@
 package org.dara.authenticationservice.service;
 
+import org.dara.authenticationservice.model.Role;
+
+import java.util.Optional;
+
 public interface RoleService {
+
+    Optional<Role> findRoleByRoleName(String roleName);
 }
--- authentication-service/src/main/java/org/dara/authenticationservice/service/impl/RoleServiceImpl.java
+++ authentication-service/src/main/java/org/dara/authenticationservice/service/impl/RoleServiceImpl.java
@@ -1,8 +1,21 @@
 package org.dara.authenticationservice.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.dara.authenticationservice.model.Role;
+import org.dara.authenticationservice.repository.RoleRepository;
 import org.dara.authenticationservice.service.RoleService;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class RoleServiceImpl implements RoleService {
+
+    private final RoleRepository roleRepository;
+
+    @Override
+    public Optional<Role> findRoleByRoleName(String roleName) {
+        return roleRepository.findByRoleName(roleName);
+    }
 }
