e6491858e0ce3929ce4819096b3a800b7bb8705d
AlirezaDaraei <daraeialireza76@gmail.com>
2026-07-11 01:24:03 +0330

    add username and userid remove expiresIn

--- authentication-service/src/main/java/org/dara/authenticationservice/dto/AuthResponse.java
+++ authentication-service/src/main/java/org/dara/authenticationservice/dto/AuthResponse.java
@@ -1,8 +1,9 @@
 package org.dara.authenticationservice.dto;
 
 public record AuthResponse(
+        Long userId,
+        String username,
         String accessToken,
         String refreshToken,
-        String tokenType,
-        Long expiresIn
+        String tokenType
 ) {}
