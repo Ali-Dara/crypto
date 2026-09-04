cd91064d626479e507e246aabd5514724c4eb391
AlirezaDaraei <daraeialireza76@gmail.com>
2026-09-05 01:52:52 +0330

    add spring doc

---
+++ wallet-service/src/main/java/org/dara/walletservice/config/OpenApiConfig.java
@@ -0,0 +1,31 @@
+package org.dara.walletservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI walletServiceOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Wallet Service")
+                        .version("1.0.0")
+                        .description("REST API for managing user wallets and balances.")
+                )
+                .components(
+                        new Components()
+                                .addSecuritySchemes("bearerAuth",
+                                            new SecurityScheme()
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                        )
+                );
+    }
+}
